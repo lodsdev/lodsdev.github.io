4e3872d6b9bdc56149890d41a1ec6316a9bbff70
LODS <lodsofcc@gmail.com>
2022-09-17 11:50:03 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from './components/nav/Nav'
+import Header from './components/header/Header'
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Nav />
+        </>
+    )
+}
+
+export default App
